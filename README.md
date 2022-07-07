16cac34a78b2ac2781623f883715059acf57e18c
Carloseduardo1984 <carloseduardo1984@gmail.com>
2022-07-07 16:23:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Criando uma documentação com SWAGGER para projetos NodeJS
+
+Tutorial para gerar automaticamente API docs
+
+Referencias: 
+* `npm swagger-jsdocs`
+* `npm swagger-ui-express` 
+* https://swagger.io/ 
+
+Github > 
+
+## Etapas
+
+* Baixe o projeto do github e extraia para seu computador
+* Abra o VsCode e abra a pasta do arquivo salvo acima
+* `npm install`
+* `npm install -save swagger-jsdoc swagger-ui-express express nodemon`
+* `npm start`
+
+* Abra um  browser e acesse sua documentação:  http://localhost:5000/api-docs.
---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Library API",
+      version: '1.0.0',
+    },
+  },
+  apis: ["app.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     description: Get all books
+ *     responses:
+ *       200:
+ *         description: Success
+ * 
+ */
+app.get('/books', (req, res) => {
+    res.send([
+        { id: 1, title: "Harry Potter 1", },
+        { id: 2, title: "Harry Potter 2", },
+        { id: 3, title: "Harry Potter 3", }
+
+    ])
+});
+
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     description: Get all books
+ *     parameters:
+ *      - name: title
+ *        description: title of the book
+ *        in: formData
+ *        required: true
+ *        type: string
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+app.post('/books', (req, res) => {
+  res.status(201).send();
+});
+
+app.listen(5000, () => console.log("listening on 5000"));
